3b129427712cdaaed45fb8c3a0a9f88fc6d4a11d
TylerBo12 <Tylershovel@gmail.com>
2023-05-09 13:10:58 -0400

    haha its yifan >:)

---
+++ SQL/src/views/AboutView.vue
@@ -0,0 +1,12 @@
+<template></template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    font-size: 2rem;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
