8456ebf5039f5bdbdb90e49d4321ba7fc128a227
YifanZhang4 <bookworm.with.a.problem@gmail.com>
2023-05-10 13:14:24 -0400

    started

---
+++ SQL/src/components/LoginForm.vue
@@ -0,0 +1,18 @@
+<template>
+<form>
+    <input type="text" name="username" id="username">
+    <input type="password" name="password" id="password">
+    <button type="submit">Enter</button>
+</form>
+</template>
+
+<script>
+    export default {
+        name: LoginForm, 
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ SQL/src/views/LoginScreen.vue
@@ -0,0 +1,13 @@
+<template>
+ <LoginForm />
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
